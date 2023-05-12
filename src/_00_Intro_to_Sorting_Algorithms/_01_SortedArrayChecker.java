2d547fe1ae53c1938f2f7edbe285c25898a3de3b
league <league@imac-14.attlocal.net>
2023-05-11 18:26:53 -0700

    started mod 2, finished sortedArrayChecker

--- src/_00_Intro_to_Sorting_Algorithms/_01_SortedArrayChecker.java
+++ src/_00_Intro_to_Sorting_Algorithms/_01_SortedArrayChecker.java
@@ -9,8 +9,12 @@ public class _01_SortedArrayChecker {
      * and false otherwise.
      */
     public static boolean intArraySorted(int[] arr) {
-        
-        return false;
+        for (int i = 1; i < arr.length; i++) {
+			if(arr[i]<arr[i-1]) {
+				return false;
+			}
+		}
+        return true;
     }
 
     /*
@@ -20,8 +24,14 @@ public class _01_SortedArrayChecker {
      * The method returns true if the double array is in ascending order
      * and false otherwise.
      */
-
-
+public static boolean doubleArraySorted(double[] arr) {
+	  for (int i = 1; i < arr.length; i++) {
+			if(arr[i]<arr[i-1]) {
+				return false;
+			}
+}  
+return true;
+}
     /*
      * 3. Write a static method called charArraySorted.
      * 
@@ -30,7 +40,14 @@ public class _01_SortedArrayChecker {
      * order and false otherwise (You can compare characters just like
      * integers).
      */
-
+public static boolean charArraySorted(char[] arr) {
+	  for (int i = 1; i < arr.length; i++) {
+			if(arr[i]<arr[i-1]) {
+				return false;
+			}
+}  
+return true;
+}
 
     /*
      * 4. Write a static method called stringArraySorted.
@@ -39,5 +56,12 @@ public class _01_SortedArrayChecker {
      *  The method returns true if the String array is in alphabetical
      *  order and false otherwise (Use the compareTo(String) method).
      */
-
+public static boolean stringArraySorted(String[] strings) {
+	  for (int i = 0; i < strings.length-1; i++) {
+			if(strings[i].compareTo(strings[i+1])>0) {
+				return false;
+			}
+}  
+return true;
+}
 }
