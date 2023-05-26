955feb2bf40252c0d58dede479f2fc0355269a26
league <league@imac-14.attlocal.net>
2023-05-25 18:27:16 -0700

    working on algorithms.java and 'searching and sorting'

--- src/_00_Intro_to_Sorting_Algorithms/_02_SwappingDigits.java
+++ src/_00_Intro_to_Sorting_Algorithms/_02_SwappingDigits.java
@@ -4,42 +4,45 @@ import java.util.Iterator;
 
 class _02_SwappingDigits {
 
-    // 1. Complete the method so that the first two elements of the array are
-    //    swapped.
-    public static void swapArrayOfTwo(int[] arr) {
-int a= arr[0];
-int b= arr[1];
-int temp=a;
-arr[0]=b;
-arr[1]=temp;
+	// 1. Complete the method so that the first two elements of the array are
+	// swapped.
+	public static void swapArrayOfTwo(int[] arr) {
+		int a = arr[0];
+		int b = arr[1];
+		int temp = a;
+		arr[0] = b;
+		arr[1] = temp;
 
-    }
+	}
 
-    // 2. Complete the method so that it sorts the array using a bubble sort.
-    //    Iterate through the array and when you find two elements that are out
-    //    of order, swap them. Repeat this until the array is in order.
-    public static void sortIntArray(int[] arr) {
-for (int i = 1; i < arr.length+1; i++) {
-	if(arr[i]< arr[i-1]) {
-		for(int k=i; k<=0; k--) {
-			if(arr[k]> arr[k-1]) {
-			break;
+	// 2. Complete the method so that it sorts the array using a bubble sort.
+	// Iterate through the array and when you find two elements that are out
+	// of order, swap them. Repeat this until the array is in order.
+	public static void sortIntArray(int[] arr) {
+		int n = arr.length;
+		boolean swapped;
+		for (int i = 0; i < n - 1; i++) {
+			swapped = false;
+			for (int k = 0; k < n - i - 1; k++) {
+				if (arr[k] > arr[k + 1]) {
+					int temp = arr[k];
+					arr[k] = arr[k + 1];
+					arr[k + 1] = temp;
+					swapped = true;
+				}
 			}
-			int a= arr[k];
-			int b= arr[k-1];
-			int temp=a;
-			arr[k]=b;
-			arr[k-1]=temp;
+			if (!swapped) {
+				break;
+			}
 		}
 	}
-}
 
-    }
-
-    // 3. Complete the method so that it finds the middle number in the array.
-    //    *Hint* it helps to sort it first.
-    //    *Double Hint* Use the method you already wrote in step 2 to sort it
-    public static int findMiddle(int[] arr) {
-        return 0;
-    }
+	// 3. Complete the method so that it finds the middle number in the array.
+	// *Hint* it helps to sort it first.
+	// *Double Hint* Use the method you already wrote in step 2 to sort it
+	public static int findMiddle(int[] arr) {
+		sortIntArray(arr);
+		int middleNumber= arr.length/2;
+		return arr[middleNumber];
+	}
 }
--- src/_03_More_Algorithms/Algorithms.java
+++ src/_03_More_Algorithms/Algorithms.java
@@ -12,6 +12,33 @@ public class Algorithms {
     }
     
     public static int findBrokenEgg(List<String> eggs) {
-        return 0;
+        for (int i = 0; i < eggs.size(); i++) {
+        if(eggs.get(i).equals("cracked")) {
+            return i;
+        }
+        }
+return 0;
     }
+
+	public static Object countPearls(List<Boolean> oysters) {
+int pearls=0;
+for (int i = 0; i < oysters.size(); i++) {
+	if (oysters.get(i).equals(true)) {
+		pearls++;
+
+	}
+	
+}
+		return pearls;
+	}
+
+	public static double findTallest(List<Double> peeps) {
+double tallest=peeps.get(0);
+for (double height:peeps) {
+	if(height>tallest) {
+		tallest=height;
+	}
+}
+return tallest;
+}
 }
