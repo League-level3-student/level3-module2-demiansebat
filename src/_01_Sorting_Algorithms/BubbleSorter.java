9297004f8f49886800773ed9e946d039b4796bc8
league <zachfitz71@gmail.com>
2023-06-01 18:32:06 -0700

    working on bubble sort

--- src/_01_Sorting_Algorithms/BubbleSorter.java
+++ src/_01_Sorting_Algorithms/BubbleSorter.java
@@ -12,6 +12,25 @@ public class BubbleSorter extends Sorter{
      */
     @Override
     void sort(int[] array, SortingVisualizer display) {
-        
+    	int i, j, temp;
+        boolean swapped;
+        for (i = 0; i < array.length; i++) {
+            swapped = false;
+            for (j = 0; j <array.length-1; j++) {
+                if (array[j] > array[j + 1]) {
+                     
+                    // Swap arr[j] and arr[j+1]
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+ 
+            // If no two elements were
+            // swapped by inner loop, then break
+            if (swapped == false)
+                break;
+        }
     }
 }
